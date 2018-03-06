0c2abae77d7617f6998d8e2cbe62569dab5c7439
Genva <inabhishek009@gmail.com>
2018-03-06 22:21:44 +0530

    Updated ChatScreen

--- screens/ChatingScreen.js
+++ screens/ChatingScreen.js
@@ -4,14 +4,17 @@ import { ExpoLinksView } from '@expo/samples';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: '',
+    title: 'Links',
   };
 
-  render() {
+  render()
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
+        {
+          <Text>
+            Hello World !
+          </Text>
+        }
         <ExpoLinksView />
       </ScrollView>
     );
