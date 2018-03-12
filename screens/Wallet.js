f5484323932d6ae940899c29f7c0fbfcc96eea90
Genva <inabhishek009@gmail.com>
2018-03-12 08:00:34 +0530

    Resolved errors.

--- screens/Wallet.js
+++ screens/Wallet.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { ScrollView, StyleSheet } from 'react-native';
+import { ExpoLinksView } from '@expo/samples';
 
 export default class Wallet extends React.Component {
   static navigationOptions = {
@@ -7,8 +8,19 @@ export default class Wallet extends React.Component {
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <ScrollView style={styles.container}>
+        {/* Go ahead and delete ExpoLinksView and replace it with your
+           * content, we just wanted to provide you with some helpful links */}
+        <ExpoLinksView />
+      </ScrollView>
+    );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
