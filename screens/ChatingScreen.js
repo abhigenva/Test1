e84a224496931dcb4cd84fafb293fbecab60880a
Genva <inabhishek009@gmail.com>
2018-07-25 10:44:22 +0530

    Header added to notification and settings tab.

--- screens/ChatingScreen.js
+++ screens/ChatingScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import { Container, Content, Separator, Text, Header, Title, Body } from 'native-base';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -9,9 +10,18 @@ export default class LinksScreen extends React.Component {
 
   render() {
     return (
+      <Container>
+      <Header>
+      <Body>
+        <Text>Notifications</Text>
+      </Body>
+      </Header>
+      <Content>
       <ScrollView style={styles.container}>
         <Text>Waiting for a Match ?</Text>
       </ScrollView>
+      </Content>
+      </Container>
     );
   }
 }
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import { ScrollView, StyleSheet, ListView } from 'react-native';
+import { ScrollView, StyleSheet, ListView, View } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
-import { Container, Content, Spinner, Input, Icon, Item, Button, Text, Header } from 'native-base';
+import { Container, Content, Separator, Text, Header, Title, Body } from 'native-base';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-
-
+    header: null,
   };
+
     render() {
     return (
-      <ScrollView style={styles.container}>
         <Container>
-        <Content>
-        <Header searchBar rounded>
-        <Item>
-        <Input placeholder="Search"/>
-        <Icon name="ios-people"/>
-        </Item>
+        <Header>
+          <Body>
+            <Text>Settings</Text>
+          </Body>
         </Header>
+        <Content>
+        <View style={styles.container}>
         <Text>Waiting for a Match ?</Text>
+        </View>
         </Content>
         </Container>
-      </ScrollView>
     );
   }
 }
@@ -30,7 +29,8 @@ export default class LinksScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
     paddingTop: 15,
-    backgroundColor: '#fff',
+
   },
 });
