454591ac72a50d17f5aca3fc38d29ddce8e141d0
Genva <inabhishek009@gmail.com>
2018-08-02 23:52:43 +0530

    Chatting tab edited.

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -12,7 +11,8 @@ export default class LinksScreen extends React.Component {
       <ScrollView style={styles.container}>
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        <Text>Hello</Text>
+        <Text>First Message.</Text>
       </ScrollView>
     );
   }
